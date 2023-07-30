93860473e28bb1611f6b8f6b2c64e6911129fa22
olexandr2009 <hristevich.ua@gmail.com>
2023-07-30 11:07:07 +0300

    number = 30;

--- src/main/java/org/example/TaskTwo.java
+++ src/main/java/org/example/TaskTwo.java
@@ -2,17 +2,17 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class TaskTwo {
-    private static  int number = new Scanner(System.in).nextInt();
-    private static volatile List<String> toPrint = new ArrayList<>();
+    private static final int number = 30;
+    private static volatile List<String> numbers = new ArrayList<>(number);
 
     static {
         for (int i = 1; i <= number; i++) {
-            toPrint.add(String.valueOf(i));
+            numbers.add(String.valueOf(i));
         }
     }
 
@@ -39,29 +39,32 @@ public class TaskTwo {
         threadD.start();
     }
 
-    private static void number() {
-        System.out.println(
-                toPrint.stream()
-                .collect(Collectors.joining(", "))
+    public static void number() {
+        int i = 0;
+        while (i < number - 1){
+            if (numbers.get(i) != null){
+                System.out.printf("%s, ", numbers.get(i));
+                i++;
+            }
+        }
+        System.out.println(numbers.get(numbers.size() - 1)
         );
     }
-
-    private static void fizzbuzz() {
+    public static void fizzbuzz() {
         replace( i -> i % 5 == 0 && i % 3 == 0,"fizzbuzz");
     }
-
-    private static void buzz() {
+    public static void buzz() {
         replace( i -> i % 5 == 0,"buzz");
     }
-
-    private static void fizz() {
+    public static void fizz() {
         replace( i -> i % 3 == 0,"fizz");
     }
     private static void replace(Predicate<Integer> predicate,String toReplace){
+        Objects.requireNonNull(toReplace);
         synchronized (TaskTwo.class) {
             for (int i = 1; i <= number; i++) {
                 if (predicate.test(i)) {
-                    toPrint.set(i - 1, toReplace);
+                    numbers.set(i - 1, toReplace);
                 }
             }
         }
